05cac96bb23b6213d0e87a13a22bb757a26fe6e2
The-Roxxstar <followinstagram296@gmail.com>
2025-01-24 15:40:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-10022031259
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/i_popcorn_movie_update/31') # How to open tutorial link for verification
 
 # Shortner 
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'f71e79189c558eb6dd0b3681899153dc41f85c52')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/i_popcorn_movie_update/31') # Tutorial video link for opening shortlink website 
